b574591e6dbc630e5ad421b9bfec43ab9c740a06
Andres Ferrari <andresferrari590@gmail.com>
2025-05-08 16:35:25 -0300

    haciendo las conexiones con mongoose

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log("Probando")
+import {Schema, model, connect } from "mongoose"
+
+process.loadEnvFile()
+
+const URI_DB= process.env.URI_DB || ""
+
+
+const connectMongo = async () =>{
+    try {
+        await connect(URI_DB)
+        console.log("Conectado a MongoDB")
+    } catch (error) {
+        console.log("Fallo la conexion a MongoDB")
+    }
+}
+
+const videoJuegoSchema = new Schema({
+    titulo: {type: String, required: true, unique:true},
+    genero:{type:String, required: true},
+    consola:{type:String, required:true},
+    Lanzamiento: {type: Number, required:true},
+})
+
+const videoJuego = model("videoJuego",  videoJuegoSchema)
+
+
+
+connectMongo()
